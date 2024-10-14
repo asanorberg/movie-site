ce87effe5f2dd5e96690f1c1482b9fadea240d4d
asanorberg <145440223+asanorberg@users.noreply.github.com>
2024-10-14 16:45:52 +0200

    seo sitemap

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { HelmetProvider } from "react-helmet-async";
 import store from "./store/store.js";
 
 import App from "./App.jsx";
@@ -8,10 +9,12 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
-      {" "}
-      {/* Wrap App with Provider and pass in the store */}
-      <App />
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        {" "}
+        {/* Wrap App with Provider and pass in the store */}
+        <App />
+      </Provider>
+    </HelmetProvider>
   </StrictMode>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPopularMovies } from "../features/movies/movieSlice";
 import MovieCard from "../components/MovieCard";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -21,6 +22,15 @@ const Home = () => {
 
   return (
     <div>
+      <Helmet>
+        <title>MovieSite | Home</title>
+        <meta
+          name="description"
+          content="Browse popular movies and find your new favorites"
+        />
+        <meta name="keywords" content="movies, popular, search, favorites" />
+      </Helmet>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 bg-darkerpurple py-10">
         {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
